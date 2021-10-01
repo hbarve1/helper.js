83f4a3338fe50c97f436af7d3d6a03cdcc1df8ec
Himank Barve <hbarve1592@gmail.com>
2021-10-01 17:59:15 +0530

    getBrowerWidth

---
+++ browsers/getBrowerWidth.js
@@ -0,0 +1,11 @@
+/** @format */
+
+var width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+
+var height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
