cfdef2523ad38ffc273bba2dcf0d6333fecbc449
Himank Barve <hbarve1592@gmail.com>
2021-10-01 17:56:12 +0530

    compareArray

---
+++ array/compareArray.js
@@ -0,0 +1,19 @@
+/** @format */
+
+// sorted array
+
+const arrayFirst = [2, 3, 1, 4, 5];
+const arraySecond = [1, 2, 3, 4, 5];
+console.log(
+  arrayFirst.length === arraySecond.length &&
+    arrayFirst.sort().every((value, index) => value === arraySecond[index])
+); //true
+
+// unsorted array
+
+const arrayFirst = [1, 2, 3, 4, 5];
+const arraySecond = [1, 2, 3, 4, 5];
+console.log(
+  arrayFirst.length === arraySecond.length &&
+    arrayFirst.every((value, index) => value === arraySecond[index])
+); // true
